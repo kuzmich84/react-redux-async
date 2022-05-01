a225934cd1684fa06aba09b2307cb3793d5667c3
Kuchukov Dmitry <kudim@inbox.ru>
2022-05-01 22:17:08 +0300

    react-redux-async

---
+++ src/components/Shop/Products.js
@@ -0,0 +1,43 @@
+import ProductItem from './ProductItem'
+import classes from './Products.module.css'
+
+const DUMMY_PRODUCTS = [
+    {
+        id: 'p1',
+        price: 6,
+        title: 'My first book',
+        description: 'The first book description',
+
+    },
+    {
+        id: 'p2',
+        price: 5,
+        title: 'My second book',
+        description: 'The second book description',
+
+    }
+]
+
+const Products = (props) => {
+    return (
+        <section className={classes.products}>
+            <h2>Buy your favorite products</h2>
+            <ul>
+                {DUMMY_PRODUCTS.map(product=> {
+                    return (
+                        <ProductItem
+                            key={product.id}
+                            id={product.id}
+                            title={product.title}
+                            price={product.price}
+                            description={product.description}
+                        />
+                    )
+                } )}
+
+            </ul>
+        </section>
+    )
+}
+
+export default Products
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import uiSlice from "./ui-slice"
+import cartSlice from "./cart-slice"
+
+const {configureStore} = require("@reduxjs/toolkit")
+
+
+const store = configureStore({
+    reducer: {
+        ui: uiSlice.reducer,
+        cart: cartSlice.reducer,
+
+    }
+})
+
+export default store
